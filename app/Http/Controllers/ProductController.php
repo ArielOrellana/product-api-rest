ddb473d41cd3acb5b02e02d50e2f6e5624dbea89
ArielOrellana <orellanaariel1998@gmail.com>
2024-07-26 13:16:08 -0300

    product controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -14,8 +14,12 @@ class ProductController extends Controller
 
     public function store(Request $request)
     {
-        $post = Product::create($request->all());
-        return response()->json($post, 201);
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'content' => 'required|string',
+        ]);
+        $product = Product::create($request->all());
+        return response()->json($product, 201);
     }
 
     public function show($id)
@@ -25,9 +29,14 @@ class ProductController extends Controller
 
     public function update(Request $request, $id)
     {
-        $post = Product::findOrFail($id);
-        $post->update($request->all());
-        return response()->json($post, 200);
+        $request->validate([
+            'title' => 'sometimes|required|string|max:255',
+            'content' => 'sometimes|required|string',
+        ]);
+        $product = Product::findOrFail($id);
+        $product->fill($request->all());
+        $product->save();
+        return response()->json($product, 200);
     }
 
     public function destroy($id)
